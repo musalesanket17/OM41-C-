82f77c5f6054ec1f8621160480a6f63c3e21f096
musalesanket17 <musalesanket17@gmail.com>
2023-01-03 19:27:26 +0530

    Test index3

---
+++ c++ Tests/index3.cpp
@@ -0,0 +1,89 @@
+Result: Pass
+Marks: 24/30
+Percentage: 73.33 %
+Questions: 10
+Correct Answers: 8
+Attempted: 10
+
+
+1. What is the index number of the last element of an array with 9 elements?
+
+      1. 9
+      2. 8
+      3. 0
+      4. 1
+
+2. Which of the following gives the address of the first element in array?
+
+     1. array[0];
+     2. array[1];
+     3. array(2);
+     4. array;
+
+3. If a copy constructor is not defined in a class then
+
+ T-> 1. Compiler itself defines one
+     2. Conpiler gives Syntax Error
+     3. Compiler gives Run time Error
+     4. None of Above
+
+4. Syntax for Copy constructor is
+
+T->  1. classname (const classname &obj) {    // body of constructor }
+     2. classname(){    // body of constructor }
+     3. Both A and B
+     4. None of above
+
+
+5. _________ is called when a new object is created from an existing object, as a copy of the existing object
+
+     1. Copy Constructor
+     2. Default Constructor
+     3. Assignment operator
+     4. None of above
+
+
+6. Default copy constructor does __________
+
+     1. Deep Copy
+ T->    2. Shallow copy
+     3. Both A and B
+     4. None of Above
+
+7. Which of the below is correct syntax to call copy constructor?
+
+      1. Employee e1 , e2;
+   T-> 2. Employee e1 = e2;
+      3. Employee e1 , e2; e1=e2
+      4. none of above
+
+8. What will be the output of the following C++ code?
+
+#include<iostream>
+using namespace std;
+int main() 
+{ 
+    int arr[2] = { 10, 20, 30, 40, 50 }; 
+    cout<<arr[1]<<endl;
+    return 0; 
+}
+
+   T-> 1. Compile time error
+      2. program will compile successfully but give one warning
+      3. 20
+      4. Both B and C
+
+9. Which of the following scenarios where deep copy is generally required?
+
+    1. If an object has pointers to dynamically allocated memory.
+    2. If dynamically allocated memory needs to be copied when the original object is copied
+    3. Only 1 is correct
+ T-> 4. Both A and B are correct
+
+10. number of constructors in cpp.
+
+
+    1. 4
+    2. 2
+ T-> 3. 3
+    4. 5
