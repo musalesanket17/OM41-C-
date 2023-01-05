421a7151492f92a94e4eb6fe3b8c8126035b6a00
musalesanket17 <musalesanket17@gmail.com>
2023-01-05 09:57:15 +0530

    Test index5

---
+++ c++ Tests/index5.cpp
@@ -0,0 +1,106 @@
+Result: Pass
+Marks: 30/30
+Percentage: 100.00 %
+Questions: 10
+Correct Answers: 10
+Attempted: 10
+
+
+1. _______________ feature of OOP indicates code reusability?
+
+     1. Encapsulation
+     2. Abstraction
+  T-> 3. Inheritance
+     4. Polymorphism
+
+2. Object oriented programming is a kind of programming in which we try to solve real world problems using?
+
+     1. Structures and functions
+  T-> 2. Class and object
+     3. Both A and B
+     4. None of the above
+
+3. ________ always describes the outer behavior of an object.
+
+   T-> 1. Abstraction
+      2. Encapsulation
+      3. Modularity
+      4. Hierarchy
+
+4. Wrapping of private data in classes in object-oriented programming languages is termed as
+
+       1. Data Encapsulation
+       2. Data hiding
+       3. Data binding
+    T-> 4. Both A and B
+
+5. function that can perform different type of tasks, where the function name remains same, which feature of OOP is used here?
+
+        1. Inheritance
+     T-> 2. Polymorphism
+        3. Abstraction
+        4. Constructor
+
+6. What is the output of this program?
+#include<iostream>
+using namespace std;
+class Person
+{
+    public:
+ Person()
+    {
+     cout <<"We are in Person class"<< endl;
+    }
+};
+
+class Student: public Person
+{
+    
+};
+
+class Faculty: public Person
+{
+    
+};
+
+int main()
+{
+     Student student;
+     Faculty faculty;
+}
+
+         1. We are in Person class
+     T->  2. We are in Person class                                   We are in Person class
+         3. Compile time error
+         4. Runtime Error
+
+
+7. Which of the following is the major pillar of OOP ?
+
+     T->1. Encapsulation
+      2. Typing
+      3. Concurrency
+      4. Persistance
+
+8. Which of the following Data members of base class inherits into derived class?
+
+      1. Public
+      2. Private , Protected
+      3. static and nonstatic
+    T-> 4. All of the above
+
+
+9. Function Overloading comes under?
+
+   T->1. Compile Time Polymorphism
+      2. Runtime Polymorphism
+      3. Abstraction
+      4. None of the Above
+
+
+10. Default mode of inheritance is
+
+   T->1. Private
+      2. Public
+      3. Protected
+      4. None
